dec19ec6f72d3617eb2a7df45c224269e4714bf5
nishio <nishio.ryota.1997@gmail.com>
2020-07-26 01:40:37 +0900

    add_最低限のカクテルの登録機能の実装

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,4 +7,20 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
     @ingredients = @cocktail.ingredients
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    cocktail = Cocktail.new(cocktail_params)
+    cocktail.save!
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :taste, :base_alcohol, :glass_type, :alcohol_percentage, :cocktail_recipe)
+  end
 end
