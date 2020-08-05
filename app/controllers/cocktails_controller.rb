326a949c98b9b791492448e4343daf08ebdd81d8
nishio <nishio.ryota.1997@gmail.com>
2020-08-05 17:41:27 +0900

    add_削除機能の実装+削除ボタンの位置変更

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -28,6 +28,11 @@ class CocktailsController < ApplicationController
     redirect_to cocktails_path
   end
 
+  def destroy
+    cocktail = Cocktail.find(params[:id])
+    cocktail.destroy
+    redirect_to cocktails_path
+  end
   private
 
   def cocktail_params
