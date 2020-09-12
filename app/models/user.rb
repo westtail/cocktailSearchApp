612d51b2d23ffff167528512af10e2a169d4be7c
nishio <nishio.ryota.1997@gmail.com>
2020-09-13 02:10:16 +0900

    add_アカウント有効化のためのモデル変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,33 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
 
     has_many :cocktails, :dependent => :destroy
+
+    attr_accessor :remember_token, :activation_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
+
+    private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    # 渡された文字列のハッシュ値を返す
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 end
