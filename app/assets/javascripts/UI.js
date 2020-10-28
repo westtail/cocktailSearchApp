37a252307b84e95dc1a0f57dfcf3f566bb432cca
nishio <nishio.ryota.1997@gmail.com>
2020-10-29 02:04:37 +0900

    add_検索のクリア機能の非同期化

--- app/assets/javascripts/UI.js
+++ app/assets/javascripts/UI.js
@@ -1,3 +1,4 @@
+// お気に入りカクテルの開け閉め
 $(document).on('turbolinks:load',function () {
     $('.title_ucer_cocktail').on('click', () => {
         $('.user_cocktail .cocktail-list').toggle();
@@ -7,5 +8,16 @@ $(document).on('turbolinks:load',function () {
 $(document).on('turbolinks:load',function () {
     $('.title_like_cocktail').on('click', () => {
         $('.like_cocktail .cocktail-list').toggle();
+    });
+});
+
+// 検索のクリア機能
+$(document).on('turbolinks:load',function () {
+    $('#clear-form').on('click', () => {
+        $('#q_name_cont').val("");
+        $('#q_base_alcohol_cont').val("");
+        $('#q_taste_cont').val("");
+        $('#q_glass_type_cont').val("");
+        $('#q_alcohol_percentage_cont').val("");
     });
 });
