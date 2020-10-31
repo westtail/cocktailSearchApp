31d914118a5db7497aa94ae0576fbec9f6994cdf
nishio <nishio.ryota.1997@gmail.com>
2020-11-01 04:53:59 +0900

    add_factory_botのデータの追加とログイン機能のテスト

---
+++ spec/factories/cocktails.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+    factory :cocktail do
+        name {'テストネーム'}
+        base_alcohol {'テストアルコール'}
+        taste {'テストテイスト'}
+        alcohol_percentage {'テストパーセント'}
+        glass_type {'テストグラス'}
+        cocktail_recipe {'テスト'}
+        user_id {1}
+    end
+end
--- spec/system/cocktails_spec.rb
+++ spec/system/cocktails_spec.rb
@@ -1,12 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "ホーム画面のテスト", type: :system do
+  before do
+    # テストユーザーを作成
+    #FactoryBot.create(:cocktail)
+    @user_a = FactoryBot.create(:user)
+  end
+  describe "ログイン機能のテスト" do
+    before do
+      visit login_path
+      fill_in 'Email', with: 'test@test.test'
+      fill_in 'Password', with: 'password'
+      click_button 'login'
+    end
+    it "ログインが成功しカクテル検索ページに移動するか" do
+      expect(page).to have_selector 'h1', text: 'カクテル検索'
+    end
+    it "ユーザーページに移動できるか" do
+      visit user_path(@user_a.id)
+      expect(page).to have_selector 'h1', text: 'ユーザーページ'
+    end
 
-    describe "ホーム画面のレイアウト" do
-        it "ホームタイトルがカクテルホーム" do
-          visit cocktails_path
-          # have_selecter でタグを指名
-          expect(page).to have_selector 'h1', text: 'カクテル検索'
-        end
-      end
+  end
+
+  describe "ホーム画面のレイアウト" do
+    it "ホームタイトルがカクテルホーム" do
+      visit cocktails_path
+      # have_selecter でタグを指名
+      expect(page).to have_selector 'h1', text: 'カクテル検索'
+    end
+  end
+
 end
