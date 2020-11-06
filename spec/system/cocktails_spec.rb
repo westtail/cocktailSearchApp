1efa1186db1c1dd911b0b4c76093b5c7d581e1c6
nishio <nishio.ryota.1997@gmail.com>
2020-11-07 04:55:24 +0900

    add_テスト名の変更とテストデータの準備方法の変更

--- spec/system/cocktails_spec.rb
+++ spec/system/cocktails_spec.rb
@@ -1,34 +1,35 @@
 require 'rails_helper'
 
-RSpec.describe "ホーム画面のテスト", type: :system do
+RSpec.describe "機能統合テスト", type: :system do
   before do
     # テストユーザーを作成
-    @user_a = FactoryBot.create(:user)
-    @user_b = FactoryBot.create(:user, name:'テストユーザー',email: '2test@test.test',password: 'password',activated: true)
-    FactoryBot.create(:cocktail, user: @user_a)
-    FactoryBot.create(:cocktail, name:'テストカクテル', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_b)
+    @user_a = FactoryBot.create(:user, name:'テストユーザー1',email: '1test@test.test',password: 'password1',activated: true)
+    @user_b = FactoryBot.create(:user, name:'テストユーザー2',email: '2test@test.test',password: 'password2',activated: true)
+    @user_c = FactoryBot.create(:user, name:'テストユーザー3',email: '3test@test.test',password: 'password3',activated: false)
+    FactoryBot.create(:cocktail, name:'テストカクテル1', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_a)
+    FactoryBot.create(:cocktail, name:'テストカクテル2', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_b)
   end
-  describe "ログイン機能のテスト" do
+  describe "ログイン機能" do
     before do
       visit login_path
-      fill_in 'Email', with: 'test@test.test'
-      fill_in 'Password', with: 'password'
+      fill_in 'Email', with: '1test@test.test'
+      fill_in 'Password', with: 'password1'
       click_button 'login'
     end
-    it "ログインが成功しカクテル検索ページに移動するか" do
+    it "ログイン成功　カクテル検索ページに移動するか" do
       expect(page).to have_selector 'h1', text: 'カクテル検索'
     end
-    it "ユーザーページに移動できるか" do
+    it "ログイン成功　ユーザーページに移動できるか" do
       visit user_path(@user_a.id)
       expect(page).to have_selector 'h1', text: 'ユーザーページ'
     end
-    it "ログインが成功しユーザーページに移動して登録カクテルがあるか確認" do
+    it "ログインが成功しユーザーページに移動して登録カクテルがあるか" do
       visit user_path(@user_a.id)
-      expect(page).to have_content 'テストネーム'
+      expect(page).to have_content 'テストカクテル1'
     end
-    it "ログインが成功しユーザーページに移動して他のカクテルがないことを確認" do
+    it "ログインが成功しユーザーページに移動して他のカクテルがないか" do
       visit user_path(@user_a.id)
-      expect(page).to have_no_content 'テストカクテル'
+      expect(page).to have_no_content 'テストカクテル２'
     end
   end
 
