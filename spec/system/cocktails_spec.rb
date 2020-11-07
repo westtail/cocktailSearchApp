645cfd79499ef504d98c4b1f7460c8c9951c8ccd
nishio <nishio.ryota.1997@gmail.com>
2020-11-08 00:07:10 +0900

    add_ログインなしの機能テスト

--- spec/system/cocktails_spec.rb
+++ spec/system/cocktails_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "機能統合テスト", type: :system do
     @user_a = FactoryBot.create(:user, name:'テストユーザー1',email: '1test@test.test',password: 'password1',activated: true)
     @user_b = FactoryBot.create(:user, name:'テストユーザー2',email: '2test@test.test',password: 'password2',activated: true)
     @user_c = FactoryBot.create(:user, name:'テストユーザー3',email: '3test@test.test',password: 'password3',activated: false)
-    FactoryBot.create(:cocktail, name:'テストカクテル1', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_a)
-    FactoryBot.create(:cocktail, name:'テストカクテル2', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_b)
+    @cocktails_a = FactoryBot.create(:cocktail, name:'テストカクテル1', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_a)
+    @cocktails_b = FactoryBot.create(:cocktail, name:'テストカクテル2', base_alcohol: 'ジン', taste: '辛い', alcohol_percentage: '40', glass_type: 'ロング',cocktail_recipe: 'テスト', user: @user_b)
   end
   describe "ログイン機能" do
     before do
@@ -33,12 +33,26 @@ RSpec.describe "機能統合テスト", type: :system do
     end
   end
 
-  describe "ホーム画面のレイアウト" do
-    it "ホームタイトルがカクテルホーム" do
-      visit cocktails_path
+  describe "ログインなし遷移機能" do
+    before do
+      visit root_path
+    end
+    it "ホーム画面に移動したか" do
       # have_selecter でタグを指名
       expect(page).to have_selector 'h1', text: 'カクテル検索'
     end
+    it "ホーム画面にテストカクテルがあるか" do
+      expect(page).to have_content 'テストカクテル1'
+    end
+    it "カクテル詳細を確認できるか" do
+      visit cocktail_path(@cocktails_a.id)
+      expect(page).to have_selector 'h1', text: 'カクテル詳細'
+    end
+    it "詳細から一覧に戻れるか" do
+      visit cocktail_path(@cocktails_a.id)
+      expect(page).to have_selector 'h1', text: 'カクテル詳細'
+      click_on "ホームへ戻る"
+      expect(page).to have_selector 'h1', text: 'カクテル検索'
+    end
   end
-
 end
