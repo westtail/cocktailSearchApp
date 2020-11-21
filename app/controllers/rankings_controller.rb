2db86623dfdd0df93a236fcd6e22609f4c324071
nishio <nishio.ryota.1997@gmail.com>
2020-11-22 04:57:21 +0900

    update_絞り込み機能の追加

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,13 +1,16 @@
 class RankingsController < ApplicationController
     def index
-        @rank_cocktails = Cocktailmark.all_ranking
-        @rank_list = ranking_number
+        @q = Cocktail.ransack(params[:q])
+        search_list = @q.result
+        rank_list = Cocktailmark.all_ranking
+        @search_rank_list = rank_list & search_list
+        @search_rank_number = ranking_number
     end
 
     private
     def ranking_number
         list = []
-        for i in @rank_cocktails do
+        for i in @search_rank_list do
             id = i.id
             number = Cocktailmark.marks_number(id)
             list << number
