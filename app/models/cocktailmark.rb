d9a3fedc67fa308265cb63e21780a757c78d4e5a
nishio <nishio.ryota.1997@gmail.com>
2020-11-22 16:21:58 +0900

    add_テスト追加とエラー解消

--- app/models/cocktailmark.rb
+++ app/models/cocktailmark.rb
@@ -4,7 +4,7 @@ class Cocktailmark < ApplicationRecord
   validates :user_id, uniqueness: { scope: :cocktail_id } # 追加
 
   def self.all_ranking
-    Cocktail.find(Cocktailmark.group(:cocktail_id).order('count(cocktail_id) desc').limit(12).pluck(:cocktail_id))
+    Cocktail.find(Cocktailmark.group(:cocktail_id).order(Arel.sql('count(cocktail_id) desc')).limit(12).pluck(:cocktail_id))
   end
 
   def self.marks_number(id)
