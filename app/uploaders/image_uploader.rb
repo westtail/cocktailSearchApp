41710bee55d446dcf0daf474e4d7a73089e8918e
nishio <nishio.ryota.1997@gmail.com>
2020-12-07 03:56:56 +0900

    add_s3での画像アップロードと表示

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,18 +1,18 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  storage :file
+  storage :fog
 
   #上限変更
-  process :resize_to_limit => [250, 405]
+  #process :resize_to_limit => [250, 405]
 
   #JPGで保存
-  process :convert => 'jpg'
+  #process :convert => 'jpg'
 
   #サムネイルを生成
-  version :thumb do
-    process :resize_to_limit => [250, 405]
-  end
+  #version :thumb do
+  #  process :resize_to_limit => [250, 405]
+  #end
 
   # jpg,jpeg,gif,pngのみ
   def extension_white_list
@@ -20,20 +20,24 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   #ファイル名を変更し拡張子を同じにする
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' 
-  end
+  #def filename
+  #  super.chomp(File.extname(super)) + '.jpg'
+  #end
 
 #日付で保存
-  def filename
-    if original_filename.present?
-      time = Time.now
-      name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-      name.downcase
-    end
-  end
+  #def filename
+  #  if original_filename.present?
+  #    time = Time.now
+  #    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+  #    name.downcase
+  #  end
+  #end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def filename
+    original_filename if original_filename
   end
 end
